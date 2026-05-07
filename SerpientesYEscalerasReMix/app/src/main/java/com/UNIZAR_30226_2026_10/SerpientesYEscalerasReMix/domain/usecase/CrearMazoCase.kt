2683eac89102e6a14e8c414d171dd487bede0d29
Iothz <137091658+Iothz@users.noreply.github.com>
2026-05-07 18:26:19 +0200

    [FIX] Crear mazos ahora pide mazo como param

--- SerpientesYEscalerasReMix/app/src/main/java/com/UNIZAR_30226_2026_10/SerpientesYEscalerasReMix/domain/usecase/CrearMazoCase.kt
+++ SerpientesYEscalerasReMix/app/src/main/java/com/UNIZAR_30226_2026_10/SerpientesYEscalerasReMix/domain/usecase/CrearMazoCase.kt
@@ -1,9 +1,10 @@
 package com.UNIZAR_30226_2026_10.SerpientesYEscalerasReMix.domain.usecase
 
+import com.UNIZAR_30226_2026_10.SerpientesYEscalerasReMix.domain.model.Mazo
 import com.UNIZAR_30226_2026_10.SerpientesYEscalerasReMix.domain.repository.MazosRepository
 
 class CrearMazoCase(private val mazosRepository: MazosRepository) {
-    suspend operator fun invoke(nombre: String) {
-        mazosRepository.crearMazo(nombre)
+    suspend operator fun invoke(nuevoMazo: Mazo): Boolean {
+        return mazosRepository.crearMazo(nuevoMazo)
     }
 }
