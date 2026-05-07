938b3c1440a0b758985b0e1979770ee29c040fd9
Iothz <137091658+Iothz@users.noreply.github.com>
2026-05-07 23:40:18 +0200

    [FIX] Revertidos cambios que borre por error

--- SerpientesYEscalerasReMix/app/build.gradle.kts
+++ SerpientesYEscalerasReMix/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     // Jetpack Compose
     buildFeatures {
         compose = true
+        buildConfig = true // Cambiar en release
     }
 
     compileOptions {
@@ -81,4 +82,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0") // Para JSON
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") // Para debug
+    implementation("com.github.franmontiel:PersistentCookieJar:v1.0.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") // LOGING
 }
